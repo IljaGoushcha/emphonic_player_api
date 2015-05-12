08cfedb64e6b90cae89a0aac65f9437e76e2add8
IljaGoushcha <iljagou@yahoo.com>
2015-05-12 11:39:15 -0400

    fixed empty playlist number of songs issue

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -6,15 +6,8 @@ class Playlist < ActiveRecord::Base
   def self.order_and_fill_playlist_songs(playlist_id)
     ordered_playlist_songs = PlaylistSong.where(playlist_id: playlist_id.to_s).order(cell_number: :asc)
 
-    binding.pry
-
-    last_cell_number = ordered_playlist_songs.last.cell_number
-    ordered_songs = ordered_playlist_songs.map do |playlist_song|
-      Song.find(playlist_song.song_id)
-    end
-
-    max_number_cells = (1+last_cell_number/15)*15
-    filled_ordered_songs_array = Array.new(max_number_cells, {
+    if ordered_playlist_songs.length == 0
+      filled_ordered_songs_array = Array.new(15, {
         amazon_uid: nil,
         name: nil,
         artist: nil,
@@ -24,12 +17,31 @@ class Playlist < ActiveRecord::Base
         fade_in_time: 0,
         fade_out_time: nil
       })
+    else
+      last_cell_number = ordered_playlist_songs.last.cell_number
+      ordered_songs = ordered_playlist_songs.map do |playlist_song|
+        Song.find(playlist_song.song_id)
+      end
 
-    i = 0
-    while i < ordered_playlist_songs.length do
-      filled_ordered_songs_array[ordered_playlist_songs[i].cell_number-1] = ordered_songs[i]
-      i +=1
+      max_number_cells = (1+last_cell_number/15)*15
+      filled_ordered_songs_array = Array.new(max_number_cells, {
+          amazon_uid: nil,
+          name: nil,
+          artist: nil,
+          album: nil,
+          pitch: 0,
+          volume: 100,
+          fade_in_time: 0,
+          fade_out_time: nil
+        })
+
+      i = 0
+      while i < ordered_playlist_songs.length do
+        filled_ordered_songs_array[ordered_playlist_songs[i].cell_number-1] = ordered_songs[i]
+        i +=1
+      end
     end
+
     my_playlist = Playlist.find(playlist_id)
 
     new_playlist = {
